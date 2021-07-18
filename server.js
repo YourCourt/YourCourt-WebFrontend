ccf906e26f771ee870ef9c6546bbaf9a6c4a8709
Juan Noguerol Tirado <juanogtir@alum.us.es>
2021-07-18 21:19:23 +0200

    Corregido outputPath

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/yourcourt-web'));
+app.use(express.static(__dirname + '/dist/yourcourt'));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/yourcourt-web/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/yourcourt/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
