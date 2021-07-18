cff4bbe04ad620d8b3cde014bc21c97f74219a25
Juan Noguerol Tirado <juanogtir@alum.us.es>
2021-07-18 20:17:38 +0200

    Editado environment de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  backendEndpoint: 'https://dev-yourcourt-api.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
 
   backendEndpoint: 'https://dev-yourcourt-api.herokuapp.com'
   //backendEndpoint: 'http://localhost:8080'
